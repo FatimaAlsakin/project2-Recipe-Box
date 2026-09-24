a18288b359e05de7e145308de29078b82169814a
FatimaAlsakin <falsakiin@gmail.com>
2026-09-24 13:20:32 +0300

    refactor: remove Category model and all references

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ const authController = require("./routes/auth.routes.js");
 const indexController = require("./routes/index.routes.js");
 const recipeController = require("./routes/recipe.routes.js");
 const commentController = require("./routes/comment.routes.js");
-const categoryController = require("./routes/category.routes.js");
 
 // Middleware
 app.use(express.static('public')) // my app will serve all static files from public folder
@@ -57,7 +56,6 @@ app.use('/auth',authController)
 app.use('/',indexController)
 app.use('/recipe',recipeController)
 app.use('/comment', commentController )
-app.use('/category', categoryController )
 
 
 
